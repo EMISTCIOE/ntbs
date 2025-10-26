da45c63c3d975893c3e9eb7d66dc1042f1da37ef
Abhishek Panthee <abhishek.080bct002@tcioe.edu.np>
2025-10-26 13:30:24 +0545

    🏗 infra: Infrastructure changes | Author: Abhishek Panthee | Branch: main | 2025-10-26 13:30:24

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -1,13 +1,12 @@
-import { React, useState, useEffect } from 'react'
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import './pages.css'
-import TeamCard from '../components/team_card/TeamCard';
+import { React, useState, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./pages.css";
+import TeamCard from "../components/team_card/TeamCard";
 import { IoArrowBack } from "react-icons/io5";
-import Loader from '../components/loader/Loader';
-import Footer from '../components/footer/Footer';
-
+import Loader from "../components/loader/Loader";
+import Footer from "../components/footer/Footer";
 
 const Container = styled.div`
   width: 100%;
@@ -17,22 +16,24 @@ const Container = styled.div`
   column-gap: 1rem;
   row-gap: 3rem;
   margin-block: 3rem;
-`
+`;
 
 const Wrapper = styled.div`
   width: 275px;
-`
+`;
 
 const Committee = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}committee_members/`);
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_BASE_URL}committee_members/`
+        );
         setData(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       } finally {
         setLoading(false);
       }
@@ -45,22 +46,25 @@ const Committee = () => {
       <div className="page-header-box">
         <Link to="/">
           <button className="Btn">
-            <div className="sign"><IoArrowBack /></div>
+            <div className="sign">
+              <IoArrowBack />
+            </div>
             <div className="text">Back</div>
           </button>
         </Link>
-        <h1 className='center page-heading'>NTBS Committee</h1>
+        <h1 className="center page-heading">NTBS Committee</h1>
       </div>
-      {loading? (<Loader />) : (
+      {loading ? (
+        <Loader />
+      ) : (
         <Container>
-        {data.map((member) => (
-          <Wrapper key={member.id}>
-          <TeamCard member={member} />
-          </Wrapper>
-        ))}
-      </Container>
-      )
-      }
+          {data.map((member) => (
+            <Wrapper key={member.sno}>
+              <TeamCard member={member} />
+            </Wrapper>
+          ))}
+        </Container>
+      )}
       <Footer />
     </>
   );
