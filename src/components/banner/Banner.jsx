577950a1b50f74faafc163e4a4ee15d0087772db
Abhishek Panthee <abhishek.080bct002@tcioe.edu.np>
2025-10-26 12:38:12 +0545

    🔧 chore: Routine maintenance | Author: Abhishek Panthee | Branch: main | 2025-10-26 12:38:12

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import './banner.css'
-import ImageSlider from '../image_slider/image_slider'
-import Slider from '../slider/slider';
-import styled from 'styled-components';
+import React from "react";
+import "./banner.css";
+import Slider from "../slider/slider";
 
 const Banner = () => {
   const smoothScroll = (targetId) => {
     const targetElement = document.getElementById(targetId);
 
     if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'smooth' });
+      targetElement.scrollIntoView({ behavior: "smooth" });
     }
   };
   return (
@@ -18,14 +16,22 @@ const Banner = () => {
       <Slider />
       <div className="home" id="home">
         <div className="main-text">
-          <h1 className='animate__animated animate__fadeInUp animate__duration-2s'>Connecting Students of Terai</h1>
-          <h2 className='animate__animated animate__fadeInUp animate__duration-2s animate__delay-1s'>Through Culture and Celebration</h2>
-          <a onClick={() => smoothScroll('about')} className='animate__animated animate__duration-2s animate__fadeInUp animate__delay-2s'>
+          <h1 className="animate__animated animate__fadeInUp animate__duration-2s">
+            Connecting Students of Terai
+          </h1>
+          <h2 className="animate__animated animate__fadeInUp animate__duration-2s animate__delay-1s">
+            Through Culture and Celebration
+          </h2>
+          <a
+            href="#about"
+            onClick={() => smoothScroll("about")}
+            className="animate__animated animate__duration-2s animate__fadeInUp animate__delay-2s"
+          >
             <button className="btn">Know More</button>
           </a>
         </div>
       </div>
     </>
-  )
-}
-export default Banner
+  );
+};
+export default Banner;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,76 +1,97 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-    const [colorChange, setColorchange] = useState(false);
-    const changeNavbarColor = () => {
-        if (window.scrollY >= 80) {
-            setColorchange(true);
-        }
-        else {
-            setColorchange(false);
-        }
-    };
-    window.addEventListener('scroll', changeNavbarColor);
-
-    const [active, setActive] = useState("nav-list");
-    const [toggle, setToggle] = useState("menu");
-    const openMenu = () => {
-        setActive('nav-list');
-        active === 'nav-list' ? setActive('nav-list menu-active') : setActive('nav-list')
-        toggle === "menu" ? setToggle('menu openmenu') : setToggle('menu');
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
     }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
 
-    function redirectToPage(page) {
-        window.location.href = page;  // Redirect to the specified page
+  const [active, setActive] = useState("nav-list");
+  const [toggle, setToggle] = useState("menu");
+  const openMenu = () => {
+    setActive("nav-list");
+    active === "nav-list"
+      ? setActive("nav-list menu-active")
+      : setActive("nav-list");
+    toggle === "menu" ? setToggle("menu openmenu") : setToggle("menu");
+  };
+
+  function redirectToPage(page) {
+    window.location.href = page; // Redirect to the specified page
+  }
+
+  const smoothScroll = (targetId) => {
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
     }
+  };
 
-    const smoothScroll = (targetId) => {
-        const targetElement = document.getElementById(targetId);
-    
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
+  return (
+    <>
+      <div className="header">
+        <div className={colorChange ? "navbar colorChange" : "navbar"}>
+          <div id="logo-img">
+            <a href="#main" onClick={() => smoothScroll("main")}>
+              <img src="images/logo.png" alt="NTBS" />
+            </a>
+          </div>
+          <div className={active}>
+            <ul>
+              <li>
+                <a href="#about" onClick={() => smoothScroll("about")}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#saraswati_puja"
+                  onClick={() => smoothScroll("saraswati_puja")}
+                >
+                  Saraswati Puja
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#committee_members"
+                  onClick={() => smoothScroll("committee_members")}
+                >
+                  Committee Members
+                </a>
+              </li>
+              <li>
+                <a href="/gallery" onClick={() => redirectToPage("/gallery")}>
+                  Gallery
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#contact_us"
+                  onClick={() => smoothScroll("contact_us")}
+                >
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div>
 
-    return (
-        <>
-            <div className='header'>
-                <div className={colorChange ? 'navbar colorChange' : 'navbar'}>
-                    <div id="logo-img">
-                        <a onClick={() => smoothScroll('main')}><img src="images/logo.png" alt="NTBS" /></a>
-                    </div>
-                    <div className={active}>
-                        <ul>
-                            <li>
-                                <a onClick={() => smoothScroll('about')}>About</a>
-                            </li>
-                            <li>
-                                <a onClick={() => smoothScroll('saraswati_puja')}>Saraswati Puja</a>
-                            </li>
-                            <li>
-                                <a onClick={() => smoothScroll('committee_members')}>Committee Members</a>
-                            </li>
-                            <li>
-                                <a onClick={() => redirectToPage('/gallery')}>Gallery</a>
-                            </li>
-                            <li>
-                                <a onClick={() => smoothScroll('contact_us')}>Contact Us</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div onClick={openMenu} className={toggle} id="menu-btn">
-                        <div>
-                            <span className="line-1"></span>
-                            <span className="line-2"></span>
-                            <span className="line-3"></span>
-                        </div>
-                    </div>
-                </div>
+          <div onClick={openMenu} className={toggle} id="menu-btn">
+            <div>
+              <span className="line-1"></span>
+              <span className="line-2"></span>
+              <span className="line-3"></span>
             </div>
-        </>
-    )
-}
-export default Navbar
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Navbar;
